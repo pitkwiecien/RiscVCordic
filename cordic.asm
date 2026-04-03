3b32b031e9379f10e3cfb690de6c2b02eb8eeeae
Piotr Kwiecień <92010822+pitkwiecien@users.noreply.github.com>
2026-04-03 22:30:37 +0200

    Fixed the input message and the infinity symbol bug

--- cordic.asm
+++ cordic.asm
@@ -30,8 +30,8 @@ atan_table: .word 0x20000000,                  # hex(floor(arctan(Power[2,-i]) *
                   0x2,
                   0x1
 iterations: .byte 30
-k: .word 0x4dba76d7             # hex(floor(Product[Divide[1,Sqrt[1+Power[2,-2i]]],{i,0,?}] * Power[2,31]))
-input_msg: .asciz "Enter the angle in degrees, in the following range: <-90, 90> to calculate its sine and cosine using CORDIC: "
+k: .word 0x4dba76d7             # hex(floor(Product[Divide[1,Sqrt[1+Power[2,-2i]]],{i,0,infinity}] * Power[2,31]))
+input_msg: .asciz "Enter the value of angle in degrees multiplied by 2^32/180 to calculate its sine and cosine using CORDIC: "
 sin_msg: .asciz "sin: "
 cos_msg: .asciz ", cos: "
 
